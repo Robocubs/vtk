ff5ac518c6ff9a8050eb9ecb14e26f533068deff
Nicholas Hubbard <hubbardn95@gmail.com>
2019-04-08 15:17:19 -0400

    Fix CUDA preprocessor

--- vtk/preprocessors/cuda.py
+++ vtk/preprocessors/cuda.py
@@ -1,4 +1,7 @@
 from .base import BasePreprocessor
+from typing import Union
+from tensorflow import test as tftest
+import numpy as np
 import cv2
 
 
@@ -9,15 +12,26 @@ class CudaPreprocessor(BasePreprocessor):
     Do not use this class unless you understand the implications, mainly converting cv2.cuda_GpuMat to numpy.ndarray and
     back having a very high amount of latency. Use the generic preprocessor instead.
     """
+    def __init__(self, image: Union[np.ndarray, cv2.cuda_GpuMat]):
+        if not tftest.is_gpu_available():
+            raise SyntaxError("Cannot use CUDA when CUDA is not present. You need both CUDA and a version of OpenCV 4 "
+                              "compiled from source.")
+        if not isinstance(image, cv2.cuda_GpuMat):
+            image = cv2.cuda_GpuMat(image)
+        super().__init__(image)
 
     def resize(self, width: int, height: int) -> cv2.cuda_GpuMat:
         """
         Resize the given image to a specific width and height.
         """
-        return cv2.resize(self.mat, (width, height)) # pragma: no cover
+        out = cv2.cuda_GpuMat()
+        cv2.cuda.resize(self.mat, out, (width, height))  # pragma: no cover
+        return out
 
     def recolor(self, color_scheme: int) -> cv2.cuda_GpuMat:
         """
         Recolor an image to another color scheme.
         """
-        return cv2.cvtColor(self.mat, color_scheme) # pragma: no cover
+        out = cv2.cuda_GpuMat()
+        cv2.cuda.cvtColor(self.mat, out, color_scheme)  # pragma: no cover
+        return out
